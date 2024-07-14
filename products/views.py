b4821fc698cd233ec3d44f9cc4ca1a24d8978fa5
Samiksha Singh <mailtosamiksha.s@gmail.com>
2024-07-14 10:58:23 +0000

    added the category filtering the right way. Grabbed the products first and then performed various filtering functions

--- products/views.py
+++ products/views.py
@@ -7,22 +7,38 @@ from .models import Product
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
-from .models import Product
+from django.db.models import Q
+from .models import Product,Category
+
+
 
 def all_products(request):
-    query = request.GET.get('q')
+    
+    products = Product.objects.all()
 
+    # Initialize variables
+    query = request.GET.get('q')
+    categories = request.GET.getlist('category', None)
+
+    
+    if categories:
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+    
+    
     if query:
-        products = Product.objects.filter(name__icontains=query)
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
         if not products.exists():
             messages.error(request, "No products found matching your search criteria.")
-    else:
-        products = Product.objects.all()
-        if request.GET.get('q') == '':
-            messages.error(request, "Please enter a search term! currently it is empty")
+    
+    # Handle empty search query
+    if not query and request.GET.get('q') == '':
+        messages.error(request, "Please enter a search term!")
 
     context = {
         'products': products,
+        'current_categories': categories,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
