10a53f1bc9c9d23c6a6e2b7c8544888aa8e26ca1
Samiksha Singh <mailtosamiksha.s@gmail.com>
2024-07-14 07:30:43 +0000

    updated the search functionality because redirec as not working

--- products/views.py
+++ products/views.py
@@ -1,24 +1,39 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render,get_object_or_404,redirect,reverse
+from django.contrib import messages
 from .models import Product
+
 # Create your views here.
 
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from .models import Product
+
 def all_products(request):
+    query = request.GET.get('q')
 
-     query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(name__icontains=query)
+        if not products.exists():
+            messages.error(request, "No products found matching your search criteria.")
+    else:
+        products = Product.objects.all()
+        if request.GET.get('q') == '':
+            messages.error(request, "Please enter a search term! currently it is empty")
 
-     if query is None or query.strip() == '':
-        return redirect('products')
-
-     products = Product.objects.all()
-
-     if query:
-        products = products.filter(name__icontains=query)
-
-     context = {
+    context = {
         'products': products,
     }
 
-     return render(request, 'products/products.html', context)
+    return render(request, 'products/products.html', context)
+
+
+     
+
+
+        
+
+
 
 def product_detail(request,pk):
 
