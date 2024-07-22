f00cecd401672a27c6d0b1e4d7fea25d95080916
Samiksha Singh <mailtosamiksha.s@gmail.com>
2024-07-22 11:25:45 +0000

    added bag app and started working on bag contexts file

--- allday_store/settings.py
+++ allday_store/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -160,7 +162,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-samiksha92-myecomstore-jh4vnal7am7.ws.codeinstitute-ide.net'
---
+++ bag/contexts.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+from django.confimport settings
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+       
