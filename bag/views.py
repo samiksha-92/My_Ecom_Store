6f81a10a34ee7888b99310fdbe7df88f6591e955
Samiksha Singh <mailtosamiksha.s@gmail.com>
2024-07-24 08:47:35 +0000

    added add to bag functionality and imported Product model from products app

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+
+from products.models import Product
 
 # Create your views here.
 
 def view_bag(request):
     return render (request, 'bag/bag.html')
+
+def add_to_bag(request,product_id):
+
+    product = get_object_or_404(Product, pk = product_id)
+    bag = request.session.get('bag',{})
+    quantity = int(request.POST.get('quantity',1))
+
+    if product_id in bag:
+        bag['product_id'] += quantity
+    else:
+        bag['product_id'] = 1
+
+    request.session['bag'] = bag
+
+    return redirect(request.POST.get('redirect_url', 'products'))
+
