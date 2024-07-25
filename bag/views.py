ccca7901ada1db5a731fc357baac773a75aa195e
Samiksha Singh <mailtosamiksha.s@gmail.com>
2024-07-25 07:41:13 +0000

    rectified add_to_bag function. Ensuring consistent usage of integer keys, removed unwanted 'product_id': 1 entry

--- bag/views.py
+++ bag/views.py
@@ -11,14 +11,18 @@ def add_to_bag(request,product_id):
 
     product = get_object_or_404(Product, pk = product_id)
     bag = request.session.get('bag',{})
-    quantity = int(request.POST.get('quantity',1))
+    quantity = int(request.POST.get('quantity'))
+
+    product_id = int(product_id)
 
     if product_id in bag:
-        bag['product_id'] += quantity
+        bag[product_id] += quantity
     else:
-        bag['product_id'] = 1
+        bag[product_id] = quantity
 
     request.session['bag'] = bag
+    print({k: v for k, v in bag.items() if k != 'product_id'})
+    #print(request.session['bag'])
 
     return redirect(request.POST.get('redirect_url', 'products'))
 
