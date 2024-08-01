02c216df44dfe2685742b5b183b3fcafb2aae3ae
Samiksha Singh <mailtosamiksha.s@gmail.com>
2024-08-01 10:17:53 +0000

    registered models order,orderlineitem and created a signals.py file

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Order, OrderLineItem
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+    extra = 0  # Prevents Django from adding empty fields by default
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'grand_total',)
+
+    fields = ('customer', 'order_number', 'date', 'delivery_cost', 'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'customer', 'date', 'order_total', 'delivery_cost', 'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
