7c74ec330a4779e034e4176fd3196b8cbff8eb8e
Samiksha Singh <mailtosamiksha.s@gmail.com>
2024-08-05 12:00:48 +0000

    updated settings for stripe ,checkout views for calculating stripr total. Also, added event handling for errors by CI

--- allday_store/settings.py
+++ allday_store/settings.py
@@ -165,8 +165,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '') 
+
+
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-samiksha92-myecomstore-jh4vnal7am7.ws.codeinstitute-ide.net'
--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
-from products.models import Customer  # Import Customer
+from products.models import Customer  
+from bag.contexts import bag_contents
+import stripe
+from django.conf import settings
 
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     if request.method == 'POST':
         order_form = OrderForm(request.POST)
