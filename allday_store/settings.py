935e7f8195fb479d3ed59e681d97f3d294de4c44
Samiksha Singh <mailtosamiksha.s@gmail.com>
2024-08-13 11:23:06 +0000

    added requiredment to allowed hosts in settings file

--- allday_store/settings.py
+++ allday_store/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-dgi14plk1bdk8%tp=u(z*n+k9et4ld(#h81nn5#oila4q3012_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-samiksha92-myecomstore-jmr68kw7r98.ws-us114.gitpod.io','8000-samiksha92-myecomstore-jh4vnal7am7.ws.codeinstitute-ide.net','https://all-day-store-9ccb3ed718d1.herokuapp.com/','localhost']
+ALLOWED_HOSTS = ['8000-samiksha92-myecomstore-jmr68kw7r98.ws-us114.gitpod.io','8000-samiksha92-myecomstore-jh4vnal7am7.ws.codeinstitute-ide.net','https://all-day-store-9ccb3ed718d1.herokuapp.com/','all-day-store-9ccb3ed718d1.herokuapp.com','localhost']
 
 
 # Application definition
